649e3db111086a06e2578d217295af99d1569b9a
tjq <tangjiaqiang@heytea.com>
2026-03-18 11:52:35 +0800

    chore: update fix packages

--- apps/host-rsbuild-remote/src/components/ErrorBoundary.tsx
+++
@@ -1,5 +0,0 @@
-// 此组件已从 remote-reload-utils 包中导出
-// 请改用 import { ErrorBoundary } from 'remote-reload-utils';
-// 此文件仅用于向后兼容
-export { ErrorBoundary } from 'remote-reload-utils';
-export type { ErrorBoundaryProps } from 'remote-reload-utils';
